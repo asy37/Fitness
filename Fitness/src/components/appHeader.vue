6a6e58518adf0945312446a25cddc309ff9a7abe
furkanmy37 <126585961+furkanmy37@users.noreply.github.com>
2023-03-01 15:22:47 +0300

    test commit (#1)
    * test commit
    
    * test commit
    
    * test branch

---
+++ Fitness/src/components/appHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="grid grid-cols-3 gap-4 p-4 bg-blue-900 text-white">
+        <div class="flex justify-around">
+            <RouterLink to="/">Anasayfa</RouterLink>
+            <RouterLink to="/aboutus">Hakkımızda</RouterLink>
+            <RouterLink to="/ourmission">Misyonumuz</RouterLink>
+        </div>
+
+        <div class="flex items-center justify-center">
+            <input class="outline-none text-blue-900 pl-2 text-sm rounded-lg w-2/3" type="text">
+        </div>
+        
+        <div class="flex justify-center">
+            <RouterLink class="mr-4" to="/login">Giriş Yap</RouterLink>
+            <RouterLink to="/register">Kayıt Ol</RouterLink>
+        </div>
+    
+    </div>
+</template>
