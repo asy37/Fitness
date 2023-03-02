924af81fd355d40d086384ab2ecf30974350d44f
furkanmy37 <furkanmy37@gmail.com>
2023-03-02 23:59:29 +0300

    test commit

--- Fitness/src/pages/Login.vue
+++ Fitness/src/pages/Login.vue
@@ -11,7 +11,7 @@
             <div class="flex flex-col justify-center items-center h-full">
                 <div class="mb-5">
                     <input class="font-mono outline-none border-none pl-2 py-1 text-xs rounded-lg w-64 sm:w-72" placeholder="Kullanıcı Adı" type="text">
-                    <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#233876"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8Z" stroke="#233876" stroke-width="2"></path> <path d="M3 21C3.95728 17.9237 6.41998 17 12 17C17.58 17 20.0427 17.9237 21 21" stroke="#233876" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+                    <svg width="10px" height="10px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#233876"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8Z" stroke="#233876" stroke-width="2"></path> <path d="M3 21C3.95728 17.9237 6.41998 17 12 17C17.58 17 20.0427 17.9237 21 21" stroke="#233876" stroke-width="2" stroke-linecap="round"></path> </g></svg>
                 </div>
                 <div class="mb-5">
                     <input class="font-mono outline-none border-none pl-2 py-1 text-xs rounded-lg w-64 sm:w-72 " placeholder="Şifre" type="password">
