93b2e317979f5890ff4cd7b6c60e59f85aef9792
Aykut <aykutyilmaz37@gmail.com>
2023-03-06 17:06:01 +0300

    sidebar responsive

--- Fitness/src/components/template/appHeader.vue
+++ Fitness/src/components/template/appHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-3 sticky top-0 gap-4 p-2 bg-blue-900 text-white">
+    <div class="sticky top-0 gap-4 p-2 bg-blue-900 text-white">
         <div class="flex justify-around">
             <div class="bg-blue-900">
                 <RouterLink class=" rounded-md  hover:bg-blue-800" to="/">
@@ -19,27 +19,21 @@
                     </svg>
                 </RouterLink>
             </div>
-
-    <div class="grid grid-cols-3 gap-4 p-2 bg-blue-900 text-white">
-        <div class="flex justify-around">
-            <RouterLink class="p-2 rounded-md  hover:bg-blue-800" to="/">Anasayfa</RouterLink>
-            <RouterLink class="p-2 rounded-md  hover:bg-blue-800" to="/aboutus">Hakkımızda</RouterLink>
-            <RouterLink class="p-2 rounded-md  hover:bg-blue-800" to="/ourmission">Misyonumuz</RouterLink>
-</div>
-
-        <div class="flex items-center justify-center">
+                <div class="flex items-center justify-center">
 
 
-            <input class="outline-none text-blue-900 p-1 pl-2 focus:border-2 focus:border-blue-700 text-sm rounded-lg w-2/3" type="text">
+                    <input class="outline-none text-blue-900 p-1 pl-2 focus:border-2 focus:border-blue-700 text-sm rounded-lg w-2/3" type="text">
 
-        </div>
-        
-        <div class="flex justify-center">
-            <RouterLink class="mr-4 p-2 rounded-md  hover:bg-blue-800" to="/login">Giriş Yap</RouterLink>
-            <RouterLink class="p-2 rounded-md  hover:bg-blue-800" to="/register">Kayıt Ol</RouterLink>
+                 </div>
+                
+                <div class="flex justify-center">
+                    <RouterLink class="mr-4 p-2 rounded-md  hover:bg-blue-800" to="/login">Giriş Yap</RouterLink>
+                    <RouterLink class="p-2 rounded-md  hover:bg-blue-800" to="/register">Kayıt Ol</RouterLink>
+                </div>
+            
+            </div>
         </div>
     
-    </div>
 </template>
 
 
